cd22084cb4f427a380ebefcf18d823a79e5b5a1a
Jordan <jordan.kelloway@keyin.com>
2024-10-22 09:44:42 -0230

    Added get by id

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,3 +21,16 @@ exports.createUser = (req, res) => {
         res.status(201).json({ message: 'User created', id: result.insertId });
     });
 }
+
+exports.getUserById = (req, res) => {
+    const {id } = req.params;
+    const sql = 'SELECT * FROM users WHERE id = ?';
+    db.query(sql, [id], (err, results) => {
+        if(results.length === 0 || !results.length){
+            return res.status(404).json({ error: 'User not found' });
+        }
+        if (err) return res.status(500).json({ error: err.message });
+        res.json(results[0]);
+    });
+
+}
